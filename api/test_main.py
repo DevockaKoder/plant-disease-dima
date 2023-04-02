a2bdebd66946956337e1009b51b978b6722f65ef
whatiamdoing <alwayswannafly070400@mail.ru>
2023-04-02 23:57:40 +0300

    Added a positive scenario test

---
+++ api/test_main.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+healthy_leaf = open('../healthy.jpg', "rb")
+
+def test_read_main():
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert "Local server is started"
+
+
+def test_predict_positive():
+    response = client.post("/predict", files={"file": ("healthy", healthy_leaf, "image/jpeg")})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['class'] == 'Healthy'

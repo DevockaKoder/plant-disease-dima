f5012e5713f7a626ee1ce55b7a193ce7ac57176c
DevockaKoder <56919331+DevockaKoder@users.noreply.github.com>
2023-04-03 15:51:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+from keras import models
+
+app = FastAPI()
+
+img = open("./healthy.jpg")
+
+MODEL = models.load_model("./model")
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
+@app.get("/test")
+async def test():
+    return "Local server is started"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_index = int(np.argmax(predictions[0]))
+    predicted_class = CLASS_NAMES[predicted_index]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence),
+    }
